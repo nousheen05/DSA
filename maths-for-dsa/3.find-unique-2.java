2c42e6a7bdef3bd75429e768ef5cfc9bbcc191ac
nousheen05 <137412087+nousheen05@users.noreply.github.com>
2023-11-23 06:20:28 +0530

    Create 3.find-unique-2.java

---
+++ maths-for-dsa/3.find-unique-2.java
@@ -0,0 +1,16 @@
+
+// find unique number - 2
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 3, 6, -1, -3};
+        int ans = arr[0]+arr[1];
+        for(int i=2;i<arr.length;i++)
+        {
+            ans = ans + arr[i];
+        }
+        System.out.println(ans);
+    }
+}
