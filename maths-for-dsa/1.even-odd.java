eeaec5046c9d8bd4e7386902e4fca76041dafa7f
nousheen05 <137412087+nousheen05@users.noreply.github.com>
2023-11-23 05:59:50 +0530

    Create 1.even-odd.java

---
+++ maths-for-dsa/1.even-odd.java
@@ -0,0 +1,17 @@
+
+// check even or odd without modulo
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int a = 3;
+        if((a & 1) == 0)
+        {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
