cfd0f818911cd2a43cef0d95a032d1a1bfd793c2
nousheen05 <137412087+nousheen05@users.noreply.github.com>
2023-11-30 20:36:40 +0530

    Create 5.nto1&1ton.java

---
+++ Recusion/5.nto1&1ton.java
@@ -0,0 +1,19 @@
+
+// print from 1 to n
+
+class Main {
+    public static void main(String[] args) {
+        
+        print(5);
+    }
+    static void print(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        print(n-1);
+        System.out.println(n);
+    }
+}
